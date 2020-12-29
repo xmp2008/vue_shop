bce6133f19326c6acbf5a89b1249aca70dfcd336
xiemopeng <xie.mopeng@vpclub.cn>
2020-12-29 18:19:17 +0800

    完成用户列表功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,52 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Login from "../components/Login.vue";
+import Home from "../components/Home.vue";
+import Welcome from "../components/Welcome.vue";
+import User from "../components/users/User.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+const routes = [{
+        path: "/login",
+        name: "Login",
+        component: Login,
+    },
+    {
+        path: "/",
+        redirect: "/login",
+    },
+    {
+        path: "/home",
+        name: "Home",
+        component: Home,
+        redirect: "/welcome",
+        children: [{
+                path: "/welcome",
+                name: "Welcome",
+                component: Welcome,
+            },
+            {
+                path: "/users",
+                name: "User",
+                component: User,
+            }
+        ]
+    },
+];
 
 const router = new VueRouter({
-  routes
-})
+    routes,
+});
+//导航守卫，类似拦截器
+router.beforeEach((to, from, next) => {
+    //登录页，允许访问，直接放行
+    if (to.path === "/login") return next();
+    const tokenStr = window.sessionStorage.getItem("token");
+    //没有token强制跳转登录页
+    if (!tokenStr) return next("/login");
+    //有token允许访问
+    next();
+});
 
-export default router
+export default router;
